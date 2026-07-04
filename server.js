7790ccc6240c3d15ca7c1e4618da704f7d3e7d03
Carlos Eduardo da Silva Ribeiro <kaduesr@gmail.com>
2026-07-04 16:40:30 -0300

    feat: add server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const http = require("http")
-const { Server } = require("socket.io")
-const server = http.createServer((req, res) => { res.writeHead(200, {"Content-Type":"text/plain"}); res.end("ChatRealTime Server") })
-const io = new Server(server, { cors: { origin: "*" } })
-io.on("connection", (s) => { console.log("Conectado:", s.id); s.on("message", (d) => io.emit("message", d)); s.on("disconnect", () => console.log("Saiu:", s.id)) })
-server.listen(3001, () => console.log("ChatRealTime :3001"))
+const http = require("http"); const { Server } = require("socket.io");
+const server = http.createServer((req, res) => { res.writeHead(200, {"Content-Type": "text/plain"}); res.end("ChatRealTime Server Running\n"); });
+const io = new Server(server, { cors: { origin: "*" } });
+io.on("connection", (socket) => {
+  console.log("Cliente conectado:", socket.id);
+  socket.on("message", (data) => { io.emit("message", { ...data, id: socket.id }); });
+  socket.on("disconnect", () => console.log("Cliente saiu:", socket.id));
+});
+server.listen(3001, () => console.log("ChatRealTime rodando na porta 3001"));
